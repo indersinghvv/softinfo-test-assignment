d126b24f27633098e39707f5aa1e48a73c04a7ac
Inder Singh <i.sgh99@gmail.com>
2023-08-04 01:31:23 +0530

    first commit project structure

---
+++ components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="none"
+      class="block"
+    >
+      <path
+        d="M15 16.875C19.66 16.875 23.4375 13.0975 23.4375 8.4375C23.4375 3.77754 19.66 0 15 0C10.3406 0 6.5625 3.77754 6.5625 8.4375C6.5625 13.0975 10.3418 16.875 15 16.875ZM20.5723 18.75H9.375C4.19766 18.75 0 22.9471 0 28.125C0 29.1604 0.839648 30 1.82227 30H28.125C29.1604 30 29.9473 29.1604 29.9473 28.125C30 22.9453 25.8047 18.75 20.5723 18.75Z"
+        fill="white"
+      ></path>
+    </svg>
+
+    <p class="text-base text-center text-white">Users</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+import { AppHeader } from '../.nuxt/components';
+<template>
+  <div class="flex flex-wrap">
+    <div
+      class="flex flex-col items-center w-1/6 h-screen gap-6 rounded-tr-[25px] rounded-br-[25px] bg-[#702f61]"
+    >
+      <AppHeader class="mt-6" />
+      <AppHeader />
+      <AppHeader />
+    </div>
+    <div class="w-5/6 bg-slate-500 flex flex-col">
+      <!-- header -->
+      <div class="flex justify-between">
+        <div class="">06</div>
+        <div>07</div>
+      </div>
+      <div class="flex flex-col md:flex-row justify-between">
+        <div>08</div>
+        <div>09</div>
+      </div>
+    </div>
+  </div>
+</template>
